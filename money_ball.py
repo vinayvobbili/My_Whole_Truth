390c22bff703aac83f7583f442ff72aeeef550c0
vinay.vobbili <vinayvobbilichetty11@gmail.com>
2024-11-16 22:32:17 -0500

    more changes

--- money_ball.py
+++ money_ball.py
@@ -1,12 +1,8 @@
 from aging_tickets import AgingTickets
-from config import load_config
-from dotenv import load_dotenv
-
 from webex_bot.webex_bot import WebexBot
+from config import load_config
 
-load_dotenv()
 config = load_config()
-
 
 def main():
     """the main"""
@@ -14,7 +10,7 @@ def main():
     bot = WebexBot(
         config.bot_api_token,
         approved_domains=['company.com'],
-        bot_name="The Money Ball!"
+        bot_name="I am The Money Ball!"
     )
     bot.add_command(AgingTickets())
     bot.run()
